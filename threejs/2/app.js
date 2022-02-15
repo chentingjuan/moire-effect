38b7460e8ab2add131f70e411415d944e1202950
Jennifer Juan <jennifer.juan@spreeeed.com>
2022-02-15 22:23:15 +0800

    add hover, velocity

--- threejs/2/app.js
+++ threejs/2/app.js
@@ -1,6 +1,7 @@
 // import * as THREE from 'three'
 // import * as dat from 'dat.gui'
 // import gsap from 'gsap'
+// import { Effect } from "postprocessing"
 
 class Sketch {
   constructor() {
@@ -26,14 +27,15 @@ class Sketch {
 
     this.settings()
     
+    // this.currentWave = 0
     this.mouse = new THREE.Vector2(0, 0)
     this.tx = 0
     this.ty = 0
     this.easing = 0.03
     this.raycaster = new THREE.Raycaster()
+    this.waterTexture = new WaterTexture({ debug: true })
     this.initEventListener() 
 
-    // this.addCursor()
     this.addObjects()
    
     this.setupResize()
@@ -45,25 +47,77 @@ class Sketch {
   }
 
   onMouseMove(e) {
-    this.mouse = {
-      x: e.clientX - this.width/2,
-      y: - e.clientY + this.height/2
-    }
+    this.waterTexture.update()
+    // // this.mouse = {
+    // //   x: e.clientX - this.width/2,
+    // //   y: - e.clientY + this.height/2
+    // // }
+    // this.mouse = {
+    //   x: ( e.clientX / this.width ) * 2 - 1,
+    //   y: - ( e.clientY / this.height ) * 2 + 1
+    // }
 
-    const offsePart = 5
-    const dx = this.mouse.x/offsePart - this.tx
-    this.tx += dx * this.easing
-    const dy = this.mouse.y/offsePart - this.ty
-    this.ty += dy * this.easing
-    gsap.to(
-      this.cover2.position, 
-      1, 
-      {
-        x: this.tx,
-        y: this.ty,
-        z: this.cover2.position.z
-      }
-    )
+    // // const offsePart = 5
+    // // const dx = this.mouse.x/offsePart - this.tx
+    // // this.tx += dx * this.easing
+    // // const dy = this.mouse.y/offsePart - this.ty
+    // // this.ty += dy * this.easing
+    // // gsap.to(
+    // //   this.cover2.position, 
+    // //   1, 
+    // //   {
+    // //     x: this.tx,
+    // //     y: this.ty,
+    // //     z: this.cover2.position.z
+    // //   }
+    // // )
+
+    // // this.mouse = {
+    // //   x: ( (e.clientX - viewportOffset.left) / this.container.getBoundingClientRect().width ) * 2 - 1,
+    // //   y: - ( (e.clientY - viewportOffset.top) / this.container.getBoundingClientRect().height ) * 2 + 1
+    // // }
+
+    // const targetMesh = this.cover1
+    // this.raycaster.setFromCamera(this.mouse, this.camera)
+    // const intersects = this.raycaster.intersectObjects([targetMesh])
+    // // console.log(intersects)
+    // if (intersects.length > 0) {
+    //   let p = intersects[0].point;
+    //   targetMesh.material.uniforms.u_mouse.value = new THREE.Vector2(p.x, p.y);
+    //   // let fake = {
+    //   //   wHeight: 0.08,
+    //   //   wTime: 0.0,
+    //   //   wLength: 0.5,
+    //   // };
+    //   // let tl = new TimelineMax({ onUpdate: function() {
+    //   //   this.instancedMesh.material.uniforms.wHeight.value[waveIndex] = fake.wHeight;
+    //   //   this.instancedMesh.material.uniforms.wTime.value[waveIndex] = fake.wTime;
+    //   //   this.instancedMesh.material.uniforms.wLength.value[waveIndex] = fake.wLength;
+    //   // }});
+    //   // tl
+    //   //   .to(fake,5,{
+    //   //     wHeight:0,
+    //   //     wTime:0.7,
+    //   //     wLength:0.3,
+    //   //     ease: Expo.easeOut,
+    //   //   })
+    //   // this.currentWave = (this.currentWave + 1)%10
+    // }
+
+    const point = {
+      x: e.clientX / window.innerWidth,
+      y: e.clientY / window.innerHeight
+    };
+
+    this.waterTexture.addPoint(point)
+
+    // this.raycaster.setFromCamera(
+    //   {
+    //     x: (ev.clientX / window.innerWidth) * 2 - 1,
+    //     y: -(ev.clientY / window.innerHeight) * 2 + 1
+    //   },
+    //   this.camera
+    // );
   }
 
   settings() {
@@ -94,7 +148,7 @@ class Sketch {
           reader.onload = e => {
             const url = e.target.result
             image.src = url
-            this.cover1.material.map = new THREE.ImageUtils.loadTexture(url)
+            this.cover1.material.map = new THREE.TextureLoader(url)
           };
 
           reader.readAsDataURL(file)
@@ -115,7 +169,7 @@ class Sketch {
           reader.onload = e => {
             const url = e.target.result
             image.src = url
-            this.cover2.material.map = new THREE.ImageUtils.loadTexture(url)
+            this.cover2.material.map = new THREE.TextureLoader(url)
           };
 
           reader.readAsDataURL(file)
@@ -123,7 +177,8 @@ class Sketch {
         input.click()
       },
       // 'background': 'radial-gradient(circle, rgba(202,123,74,1) 0%, rgba(180,207,62,1) 100%)'
-      'background': '#ddd'
+      'background': '#ddd',
+      'rot-velocity': 1,
     };
     this.gui = new dat.GUI()
 
@@ -153,6 +208,8 @@ class Sketch {
     this.gui.add(this.settings, 'background').onChange(() => {
       document.querySelector('body').style.background = this.settings['background']
     })
+
+    this.gui.add(this.settings, 'rot-velocity').min(0.1).max(1)
   }
   
   setupResize() {
@@ -168,13 +225,63 @@ class Sketch {
   }
 
   addObjects () {
+    // const m = new THREE.MeshBasicMaterial({
+    //   map: this.waterTexture.texture,
+    //   transparent: true,
+    //   blending: THREE.AdditiveBlending,
+    //   depthTest: false,
+    //   depthWrite: false,
+    // })
+    // // m.visible = false
+    // this.scene1.add(new THREE.Mesh(new THREE.PlaneBufferGeometry(this.width, this.height, 1, 1), m))
+
     let texture = new THREE.TextureLoader().load(`../../assets/images/pattern-${this.settings['selected-1']}.png`)
     texture.wrapS = texture.wrapT = THREE.RepeatWrapping
     texture.repeat.set(1, 1)
-    this.material1 = new THREE.MeshBasicMaterial({map: texture, transparent: true})
-    this.material1.map.minFilter = THREE.LinearFilter
-    this.material1.map.needsUpdate = true
-    this.material2 = new THREE.MeshBasicMaterial({map: texture, transparent: true})
+    // this.material1 = new THREE.MeshBasicMaterial({map: texture, transparent: true})
+    // this.material1.map.minFilter = THREE.LinearFilter
+    // this.material1.map.needsUpdate = true
+    this.material1 = new THREE.ShaderMaterial({
+      uniforms: {
+        u_image: new THREE.Uniform(texture),
+        // u_image: new THREE.Uniform(this.waterTexture.texture),
+        // u_displacement: { value: null }
+        u_displacement: new THREE.Uniform(this.waterTexture.texture),
+        // u_mouse: new THREE.Uniform(new THREE.Vector2(0, 0)),
+      },
+      transparent: true,
+      fragmentShader: `
+        uniform sampler2D u_image;
+        uniform sampler2D u_displacement;
+
+        varying vec2 vUv;
+        float PI = 3.141592653589793238;
+
+        void main() {
+          vec4 displacement = texture2D(u_displacement, vUv);
+          float theta = displacement.r*2.*PI;
+
+          vec2 dir = vec2(sin(theta), cos(theta));
+
+
+          vec2 uv = vUv + dir*displacement.r*0.01;
+
+          gl_FragColor = texture2D(u_image, uv);
+        }
+      `,
+      vertexShader: `
+        varying vec2 vUv;
+        uniform vec2 u_mouse;
+
+        void main(){
+          vec3 pos = position.xyz;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.);
+          vUv = uv;
+        }
+      `
+    });
+
+    this.material2 = new THREE.MeshBasicMaterial({ map: texture, transparent: true })
     this.material2.map.minFilter = THREE.LinearFilter
     this.material2.map.needsUpdate = true
 
@@ -183,8 +290,6 @@ class Sketch {
     this.cover1 = new THREE.Mesh(planeGeometry, this.material1)
     this.cover2 = new THREE.Mesh(planeGeometry, this.material2)
     this.cover2.position.z = - this.planeSize
-    // this.cover1.rotation.y = 200
-    // this.cover2.rotation.z = Math.PI / 2
     this.scene.add(this.cover1)
     this.scene.add(this.cover2)
   }
@@ -194,7 +299,7 @@ class Sketch {
     // stats.begin()
     this.time += 0.001
 
-    this.cover2.rotation.z = this.time
+    this.cover2.rotation.z = this.time / this.settings['rot-velocity']
     // this.scroll.render()
     // this.currentScroll = this.scroll.scrollToRender
    
@@ -202,13 +307,17 @@ class Sketch {
     // this.objectsMaterial.opacity = 0.5
     // this.objectsMaterial2.uniforms.time.value = this.time
 
-    this.raycaster.setFromCamera(this.mouse, this.camera)
-    this.intersects
+    // if(this.waterTexture.points.length!==0) this.waterTexture.update()
 
 
+    // this.renderer.setRenderTarget(this.baseTexture)
+    // this.renderer.render( this.scene1, this.camera )
+    // this.material1.uniforms.u_displacement.value = this.baseTexture.texture
+    // this.renderer.setRenderTarget(null)
+    // this.renderer.clear()
+    this.renderer.render(this.scene, this.camera)
 
-    this.renderer.render( this.scene, this.camera )
-    this.renderer.setPixelRatio(window.devicePixelRatio)
+    // this.renderer.setPixelRatio(window.devicePixelRatio)
     window.requestAnimationFrame(this.render.bind(this))
     // stats.end()
   }
@@ -218,3 +327,106 @@ window.onload = () => {
   new Sketch()
 }
 
+const easeOutSine = (t, b, c, d) => {
+  return c * Math.sin((t / d) * (Math.PI / 2)) + b;
+};
+
+const easeOutQuad = (t, b, c, d) => {
+  t /= d;
+  return -c * t * (t - 2) + b;
+};
+
+class WaterTexture {
+  constructor(options) {
+    this.size = 64
+    this.points = []
+    this.radius = this.size * 0.1
+    this.width = this.height = this.size
+    this.maxAge = 2
+
+    if (options.debug) {
+      this.width = window.innerWidth
+      this.height = window.innerHeight
+      this.radius = this.width * 0.25
+    }
+
+    this.initTexture()
+    if (options.debug) document.body.append(this.canvas);
+  }
+
+  initTexture() {
+    this.canvas = document.createElement("canvas");
+    this.canvas.id = "WaterTexture";
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+    this.canvas.style.position = 'fixed'
+    this.canvas.style.left = 0
+    this.canvas.style.top = 0
+    this.canvas.style.right = 0
+    this.canvas.style.bottom = 0
+    this.canvas.style.zIndex = 999
+    this.canvas.style.opacity = 0
+    this.canvas.style.pointerEvents = 'none'
+    this.ctx = this.canvas.getContext("2d")
+    this.clear();
+
+    this.texture = new THREE.Texture(this.canvas) 
+    this.texture.needsUpdate = true;
+  }
+  clear() {
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+  addPoint(point) {
+    let force = 0;
+    let vx = 0;
+    let vy = 0;
+ 
+    this.points.push({ x: point.x, y: point.y, age: 0, force, vx, vy });
+  }
+  update() {
+    this.clear()
+    // let agePart = 1 / this.maxAge;
+    this.points.forEach((point, i) => {
+      // let slowAsOlder = 1 - point.age / this.maxAge;
+      // let force = point.force * agePart * slowAsOlder;
+      point.x += point.vx
+      point.y += point.vy
+      point.age += 1
+      if (point.age > this.maxAge) {
+        this.points.splice(i, 1)
+      }
+    })
+    this.points.forEach(point => {
+      this.drawPoint(point)
+    })
+    this.texture.needsUpdate = true
+  }
+  drawPoint(point) {
+    // Convert normalized position into canvas coordinates
+    let pos = {
+      x: point.x * this.width,
+      y: point.y * this.height
+    }
+    const radius = this.radius
+    const ctx = this.ctx
+
+    let intensity = 1
+    intensity = easeOutSine((1 - point.age / this.maxAge), 0, 1, 1)
+  
+    let color = "255,255,255";
+
+    let offset = this.width * 1;
+    // 1. Give the shadow a high offset.
+    ctx.shadowOffsetX = offset;
+    ctx.shadowOffsetY = offset;
+    ctx.shadowBlur = radius * 5;
+    ctx.shadowColor = `rgba(${color}, ${1 * intensity})`;
+
+    this.ctx.beginPath();
+    this.ctx.fillStyle = `rgba(255,0,0,1)`;
+    // 2. Move the circle to the other direction of the offset
+    this.ctx.arc(pos.x - offset, pos.y - offset, radius, 0, Math.PI * 2);
+    this.ctx.fill();
+  }
+}
